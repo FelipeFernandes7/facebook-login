2850e667b6634962c7fd1c200afd7b9af8787fd2
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-09-17 14:26:56 -0300

    improve code quality

--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -15,9 +15,15 @@ describe("FacebookApi", () => {
   });
 
   beforeEach(() => {
-    httpClient.get.mockResolvedValueOnce({
-      access_token: "any_app_token",
-    });
+    httpClient.get
+      .mockResolvedValueOnce({ access_token: "any_app_token" })
+      .mockResolvedValueOnce({ data: { user_id: "any_user_id" } })
+      .mockResolvedValueOnce({
+        id: "any_facebook_id",
+        name: "any_facebook_name",
+        email: "any_facebook_email",
+      });
+
     sut = new FacebookApi(httpClient, clientId, clientSecret);
   });
 
@@ -43,6 +49,28 @@ describe("FacebookApi", () => {
         access_token: "any_app_token",
         input_token: "any_client_token",
       },
+    });
+  });
+
+  it("should get user info", async () => {
+    await sut.loadUser({ token: "any_client_token" });
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: "https://graph.facebook.com/any_user_id",
+      params: {
+        fields: "id,name,email",
+        access_token: "any_client_token",
+      },
+    });
+  });
+
+  it("should return facebook user info", async () => {
+    const user = await sut.loadUser({ token: "any_client_token" });
+
+    expect(user).toEqual({
+      facebookId: "any_facebook_id",
+      name: "any_facebook_name",
+      email: "any_facebook_email",
     });
   });
 });
